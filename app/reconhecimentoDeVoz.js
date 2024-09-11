5629fa1e57a9b01a148c5b44320711bee12d3717
luamondstock <134516705+luamondstock@users.noreply.github.com>
2024-09-11 11:20:38 -0300

    Add files via upload

--- app/reconhecimentoDeVoz.js
+++ app/reconhecimentoDeVoz.js
@@ -1,21 +1,23 @@
 const SpeechRecognition =
   window.SpeechRecognition || window.webkitSpeechRecognition;
 
-const Elementchute = document.getElementById("chute")
+const elementoChute = document.getElementById("chute")
 const recognition = new SpeechRecognition();
 recognition.lang = "pt-br";
 recognition.start()
 
 recognition.addEventListener("result", onSpeak)
+recognition.addEventListener('end', () => 
+recognition.start())
 function onSpeak(e){
    chute = (e.results[0][0].transcript)
    exibeChute(chute)
+   verificaSeOChutePossuiUmValorValido(chute)
 }
 
 function exibeChute(chute){
-  Elementchute.innerHTML = ` 
+  elementoChute.innerHTML = ` 
   <div> Você disse:</div>
   <span class="box">${chute}</span>
-  <div>O número secreto é maior </div>
   `
 }
